f1f2914a9df707bb6070d9ed123d7f52bf8c9815
asserghazi-m <asserghazi3@gmail.com>
2025-11-28 16:54:00 +0200

    Main function to run the code

---
+++ Studentgrading.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.studentgrading;
+
+/**
+ *
+ * @author asser
+ */
+import java.util.*;
+
+
+public class Studentgrading {
+
+    public static void main(String[] args) { 
+
+            GUI G1=new GUI();
+        
+        
+    }}
+ 
+       
